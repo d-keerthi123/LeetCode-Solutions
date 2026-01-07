d9ee3f852e1cefff89441e8fe5e85c31a34b9fec
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-07 20:25:37 +0530

    Create 387_FirstUniqueCharacter.java

---
+++ Queue/387_FirstUniqueCharacter.java
@@ -0,0 +1,22 @@
+//Time Complexity - O(n)
+//Space Complexity
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int freq[]=new int[26]; // to track how many times a character is repeated in a string
+        Queue<Integer> q= new LinkedList<>(); // to store characters
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i); //gives  character at thar index
+            q.add(i);
+            freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[s.charAt(q.peek())-'a']>1){
+                q.remove();
+            }
+        }
+        //2 cases - queue becomes empty or feq==1 
+        return q.isEmpty()? -1:q.peek();
+
+    }
+}
