312cd1b4d5695ae8615daedcc5d22493a579316d
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-08 21:54:55 +0530

    Create 61_RotateLL.java

---
+++ LinkedList/61_RotateLL.java
@@ -0,0 +1,40 @@
+//Time Complexity O(n)
+//Space Complexity O(1)
+
+class Solution {
+    public ListNode findNthNode(ListNode temp,int k){
+        int count=1;
+        while(temp != null){
+            if(count ==k ){
+                return temp;
+            }
+            count++;
+            temp=temp.next;
+        }
+        
+            return temp;
+
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k==0){
+            return head;
+        }
+        int len=1;
+        ListNode tail=head;
+        while(tail.next != null){
+            len++;
+            tail=tail.next;
+        }
+        if(k % len==0){
+            return head;
+        }
+        k=k % len;
+        tail.next=head;
+        ListNode newLastNode=findNthNode(head,len-k);
+        head=newLastNode.next;
+        newLastNode.next=null;
+
+        return head;
+
+    }
+}
