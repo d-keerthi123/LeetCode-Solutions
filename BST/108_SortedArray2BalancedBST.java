0f97e18f24cb9316e5a8bebde0a976b6a3438cb7
Danda Keerthi <dandakeerthi2@gmail.com>
2026-01-26 12:12:43 +0530

    Create 108_SortedArray2BalancedBST.java

---
+++ BST/108_SortedArray2BalancedBST.java
@@ -0,0 +1,20 @@
+//Time Complexity O(n)
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums){
+        
+        return helper(nums,0,nums.length-1); 
+    }
+    public TreeNode helper(int[] nums,int start,int end) {
+        //base case
+        if(start >end){
+            return null;
+        }
+
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=helper(nums,start,mid-1);
+        root.right=helper(nums,mid+1,end);
+
+        return root;
+    }
+}
