81bf0fc7bbed185ec793754c1f08ad682e1818a5
Danda Keerthi <dandakeerthi2@gmail.com>
2026-03-03 21:14:54 +0530

    Create 295_FindMedianFromDataStream.java

---
+++ Heap/295_FindMedianFromDataStream.java
@@ -0,0 +1,47 @@
+//Space Complexity O(n)
+
+class MedianFinder {
+    private PriorityQueue<Integer> leftMaxHeap;
+    private PriorityQueue<Integer> rightMinHeap;
+
+    public MedianFinder() {
+      leftMaxHeap= new PriorityQueue<>
+       (Collections.reverseOrder()); //descending order
+
+       rightMinHeap=new PriorityQueue<>();
+
+    }
+    
+    public void addNum(int num) { //O(log n)
+        //add element to left max heap
+        if(leftMaxHeap.isEmpty() || num<leftMaxHeap.peek()){
+            leftMaxHeap.add(num);
+        }
+        else{
+            rightMinHeap.add(num);
+        }
+
+        //Always maintain left max heap with 1 element more than the right_min_heap
+        //or no. of elements should be equal in both heaps
+
+        if(leftMaxHeap.size() > rightMinHeap.size()+1){
+            rightMinHeap.add(leftMaxHeap.peek());
+            leftMaxHeap.remove();
+        }
+        //this case should never happen
+        else if(leftMaxHeap.size() < rightMinHeap.size()){ 
+            leftMaxHeap.add(rightMinHeap.peek());
+            rightMinHeap.remove();
+        }
+    }
+    
+    public double findMedian() { O(1)
+        // even
+        if(leftMaxHeap.size() == rightMinHeap.size() ) {
+            double mean= (leftMaxHeap.peek() + rightMinHeap.peek())/2.0;
+            return mean;
+        }
+
+        return leftMaxHeap.peek();
+    }
+}
