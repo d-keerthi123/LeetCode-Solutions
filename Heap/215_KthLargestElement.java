2e657c56c27d2af902fdc017631a000de3bc1df6
Danda Keerthi <dandakeerthi2@gmail.com>
2026-04-06 20:46:24 +0530

    Update 215_KthLargestElement.java

--- Heap/215_KthLargestElement.java
+++ Heap/215_KthLargestElement.java
@@ -28,4 +28,35 @@ class Solution {
         }
         return pq.peek().num;
     }
+}  
+
+
+//More efficient Approach
+//Space  Complexity O(k)--> bcz heap size is K
+// Time Complexity O(n log k) 
+
+class Solution {
+    public class Pair implements Comparable<Pair>{
+        int num;
+        int idx;
+
+        Pair(int num,int idx){
+            this.num=num;
+            this.idx=idx;
+        }
+        @Override
+        public int compareTo(Pair p2){
+            return this.num-p2.num;
+        }
+    }
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Pair> pq= new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            pq.add(new Pair(nums[i],i));
+            if(pq.size() >k ){
+                pq.remove();
+            }
+        }
+        return pq.peek().num;
+    }
 }
